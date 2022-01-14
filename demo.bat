d04c68353024988e3c9f1c99b2973ec81db6bb35
Samid <samidms@code.edu.az>
2022-01-14 23:22:59 +0400

    cmd ile bat faylin elave olunmasi

---
+++ demo.bat
@@ -0,0 +1,42 @@
+title retest vs code app authorize
+
+prompt $$
+cls
+
+rem systeminfo
+
+echo yazdigim C# programinin calishdrlmasi
+cd ..
+cd ..
+cd ..
+cd ..
+
+cd
+
+cd users
+
+cd 12345
+
+cd desktop
+
+cd Visual Code
+
+cd ConsoleApp1
+
+cd ConsoleApp1
+
+cd bin
+
+cd Debug
+
+cd net5.0
+
+start ConsoleApp1.exe
+start ConsoleApp1.exe
+start ConsoleApp1.exe
+
+
+dir
+
+rem pause
+timeout 10
